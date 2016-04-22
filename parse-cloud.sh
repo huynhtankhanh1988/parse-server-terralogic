b8fed6bf91d972cbe763fd769b6086d32a961050
mark.nguyen <mark.nguyen@infonam.com>
2016-04-22 08:21:27 +0700

    [Update] Command

---
+++ parse-cloud.sh
@@ -0,0 +1,1 @@
+PORT=8443 node bin/parse-server  --appId APPLICATION_ID --masterKey MASTER_KEY --cloud spec/cloud-terra/main.js --databaseURI mongodb://localhost:27017/dev --mountPath /
---
+++ parse-no-cloud.sh
@@ -0,0 +1,1 @@
+PORT=8443 node bin/parse-server --appId APPLICATION_ID --masterKey MASTER_KEY --databaseURI mongodb://localhost:27017/dev --mountPath /
