475d1c4beecfe7a901088dba9c66699d786fdf61
mark.nguyen <mark.nguyen@infonam.com>
2016-04-25 17:25:49 +0700

    [Update] modify code

--- spec/cloud-terra/collection-config-validation/validation-schema.js
+++ spec/cloud-terra/collection-config-validation/validation-schema.js
@@ -1,61 +1,59 @@
-
+var propertiesItem=require("./item");
+var propertiesMenuItem=require("./menu-item");
+var propertiesMenu=require("./menu");
+var propertiesSetting=require("./setting");
+var propertiesShare=require("./share");
+var propertiesStyle=require("./style");
+var cfg = require("/spec/cloud-terra/constraint-type");
 
 module.exports = {
   //validate Schema Item
   validateSchemaItem: function(obj) {
-    var propertiesItem=require("./item");
-    validate(obj,"Item", propertiesItem);
+    validate(obj,cfg.constraintType.item, propertiesItem);
   },
 
   //validate Schema Menu Item
   validateSchemaMenuItem: function(obj) {
-    var propertiesMenuItem=require("./menu-item");
-    validate(obj,"MenuItem", propertiesMenuItem);
+    validate(obj,cfg.constraintType.menuItem, propertiesMenuItem);
   },
 
   //validate Schema Menu
   validateSchemaMenu: function(obj) {
-    var propertiesMenu=require("./menu");
-    validate(obj,"Menu", propertiesMenu);
+    validate(obj,cfg.constraintType.menu, propertiesMenu);
   },
 
   //validate schema setting
   validateSchemaSetting: function(obj) {
-    var propertiesSetting=require("./setting");
-    validate(obj,"Setting", propertiesSetting);
+    validate(obj,cfg.constraintType.setting, propertiesSetting);
   },
   //validate schema setting
   validateSchemaShare: function(obj) {
-    var propertiesShare=require("./share");
-    validate(obj,"Share", propertiesShare);
+    validate(obj,cfg.constraintType.share, propertiesShare);
   },
   //validate schema setting
   validateSchemaStyle: function(obj) {
-    var propertiesStyle=require("./style");
-    validate(obj,"Style", propertiesStyle);
+    validate(obj,cfg.constraintType.style, propertiesStyle);
   }
 
 };
 
+
+//validate data public
 function validate(obj,type, properties) {
+  // properties check list
   var keyPropertiesCheck;
   var checkValidate=true;
   var model=obj.req.object.toJSON();
-  if((type==="Item"||type==="Menu" || type === "Setting" || type === "Style" ) && model && model.properties){
+  if((type===cfg.constraintType.item||type===cfg.constraintType.menu || type === cfg.constraintType.setting || type === cfg.constraintType.style ) && model && model.properties){
       keyPropertiesCheck=Object.keys(model.properties);
-  }
-  if(type==="MenuItem" && model && model.items && model.items.properties){
+  }else if(type===cfg.constraintType.menuItem && model && model.items && model.items.properties){
       keyPropertiesCheck=Object.keys(model.items.properties);
-  }
-  if(type==="Share" && model){
+  }else if(type===cfg.constraintType.share && model){
       keyPropertiesCheck=Object.keys(model);
   }
-
   if(keyPropertiesCheck){
     keyPropertiesCheck.map(function(itemProperties) {
         if(!(properties.indexOf(itemProperties) > -1)){
-          console.log("itemProperties");
-          console.log(itemProperties);
           checkValidate=false;
         }
     });
@@ -63,6 +61,6 @@ function validate(obj,type, properties) {
   if(checkValidate){
     obj.res.success();
   }else{
-    obj.res.error("Schemal invalid");
+    obj.res.error("Schemal "+type+" invalid");
   }
 };
